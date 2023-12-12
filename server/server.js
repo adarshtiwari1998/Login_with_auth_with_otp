db1f97d3ac0e931aa29c1d8c7349be95640a57e5
Adarsh Tripathi <adarshtiwari1998@gmail.com>
2023-12-12 17:05:57 +0530

    added changes in server.js file and .env file

--- server/server.js
+++ server/server.js
@@ -12,35 +12,30 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
 
+/** CORS Configuration */
+const corsOptions = {
+    origin: 'https://teal-crisp-c122c1.netlify.app',
+    optionsSuccessStatus: 200
+};
 
-const port = 8080;
+app.use(cors(corsOptions));
 
 /** HTTP GET Request */
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request");
 });
 
-
-/** api routes */
+/** API Routes */
 app.use('/api', router);
-// app.use('/api', router)
 
-/** start server only when we have valid connection */
-
-connect().then(() => {
-    try {
+/** Start server only when we have a valid connection */
+connect()
+    .then(() => {
+        const port = process.env.PORT || 8080;
         app.listen(port, () => {
-            console.log(`Server connected to https://teal-crisp-c122c1.netlify.app`);
-        })
-    } catch (error) {
-        console.log('Cannot connect to the server')
-    }
-}).catch(error => {
-    console.log("Invalid database connection...!");
-})
-
-
-// app.listen(port, () => {
-//  console.log(`Server connected to http://localhost:${port}`);
-//   })
-
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch(error => {
+        console.log("Invalid database connection...!");
+    });
